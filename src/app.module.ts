6f471c62e404ae8cdabcf3801162c3eb76a40efb
Konstantin <konstantinz@wizardsdev.com>
2024-03-14 20:59:20 +0200

    edit app

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
-import { ProductsModule } from './products/products.module';
 import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-    ProductsModule,
     MongooseModule.forRoot(
       'mongodb+srv://zakharchenkokv:11110000Kot@boostdb.efkkvhj.mongodb.net/boost?retryWrites=true&w=majority&appName=BoostDB',
     ),
