91124fc403219381d86db7b57f24aa5f9507d58f
Konstantin <konstantinz@wizardsdev.com>
2024-03-15 11:15:32 +0200

    revert users

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-// import { UsersModule } from './users/users.module';
+import { UsersModule } from './users/users.module';
 import { MongooseModule } from '@nestjs/mongoose';
-// import { AuthModule } from './auth/auth.module';
+import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-    // UsersModule,
-    // AuthModule,
+    UsersModule,
+    AuthModule,
     MongooseModule.forRoot(
       'mongodb+srv://leduc1901:melody19@kylecluster.abjzu.mongodb.net',
     ),
