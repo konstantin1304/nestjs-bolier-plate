b605005a28d453ce8cb0f0a1d4a6d6f2b6281b80
Konstantin <konstantinz@wizardsdev.com>
2024-03-15 11:52:41 +0200

    change DB

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,7 +7,7 @@ import { ConfigModule } from '@nestjs/config';
 @Module({
   imports: [
     MongooseModule.forRoot(
-      'mongodb+srv://leduc1901:melody19@kylecluster.abjzu.mongodb.net',
+      'mongodb+srv://zakharchenkokv:11110000Kot@boostdb.efkkvhj.mongodb.net/boost?retryWrites=true&w=majority&appName=BoostDB',
     ),
     ConfigModule.forRoot(),
   ],
