d47eb029757ac3109322d8eb73c975baf74e5fff
Konstantin <konstantinz@wizardsdev.com>
2024-03-20 11:24:31 +0200

    fix cors

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
-  app.enableCors({
-    origin: '*',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    allowedHeaders: 'Content-Type, Accept',
-  });
+
+  const corsOptions: CorsOptions = {
+    origin: '*', // You can change this to your specific origins
+    methods: ['GET', 'PUT', 'POST', 'DELETE', 'OPTIONS'], // Specify the HTTP methods that are allowed
+    allowedHeaders: '*', // Specify which headers can be sent in the request
+    exposedHeaders: '*', // Specify the headers that the client is allowed to access
+  };
+
+  app.enableCors(corsOptions);
   await app.listen(5019);
 }
 bootstrap();
